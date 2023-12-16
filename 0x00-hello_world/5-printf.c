47ca3749923353be7f156033ea820fde847d100a
mohamed <abdoumoha743@gmail.com>
2023-12-16 13:46:48 -0800

    ass

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - entry pont of the program
  *
@@ -8,6 +9,6 @@
  */
 	int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
