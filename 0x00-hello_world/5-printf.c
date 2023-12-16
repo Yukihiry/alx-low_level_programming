48c9bcbdbc8179847b760e5e6b6eaa19101cbdc0
Mohamed12 <63963049+Yukihiry@users.noreply.github.com>
2023-12-16 22:41:51 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+* main - entry point of the program
+* description: function printf
+* return always 0 (success)
+    
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
