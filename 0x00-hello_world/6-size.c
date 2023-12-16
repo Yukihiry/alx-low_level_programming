6f69429b9b02f2b5679e8a43e86d888c6f667234
Mohamed12 <63963049+Yukihiry@users.noreply.github.com>
2023-12-16 23:47:50 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,7 @@
 /**
  * main - Entry oint of the program
  *
+ *description: using sizeof function
  * Return always 0 (success)
  */
 int main(void)
@@ -11,5 +12,5 @@ int main(void)
 	printf("size of an int: %lu byte(s)\n",sizeof(int));
 	printf("size of a long int: %lu byte(s)\n",sizeof(long int));
 	printf("size of a long long int: %lu byte(s)\n",sizeof(long long int));
-	printf("size of a float: %lu byte(s)\n",sizeof(float))
+	printf("size of a float: %lu byte(s)\n",sizeof(float));
 	return (0);
