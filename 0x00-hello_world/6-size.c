fc7845446a6cb68a337a3b4b2c82e80596f0edba
mohamed <abdoumoha743@gmail.com>
2023-12-16 14:37:21 -0800

    i win

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry oint of the program
+ *
+ * Return always 0 (success)
+ */
+int main(void)
+{
+	printf("size of a char:",sizeof(char));
+	printf("size of an int:",sizeof(int));
+	printf("size of a long int:",sizeof(long int));
+	printf("size of a long long int:",sizeof(long long int));
+	printf("size of a float:",sizeof(float))
+	return (0);
