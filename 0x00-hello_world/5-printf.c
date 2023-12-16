2c6d9fa7be4e48d1ac69c95564fe5ac6450ab70c
mohamed <abdoumoha743@gmail.com>
2023-12-16 13:33:29 -0800

    sw

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry pont of the program
+ *
+ * desecription: function printf 
+ *
+ * return always 0 (success)
+ */
+	int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
