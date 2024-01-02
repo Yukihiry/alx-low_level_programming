978e57346b0a91fb6cbca8dd1525f52f837871d7
Mohamed12 <63963049+Yukihiry@users.noreply.github.com>
2024-01-02 14:55:09 +0100

    Create 7-main.c

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    // Step 1: Define a string
+    str = "0123456789";
+
+    // Step 2: Call the puts_half function
+    puts_half(str);
+
+    // Step 3: Return 0
+    return (0);
+}
